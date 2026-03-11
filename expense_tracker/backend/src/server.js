db9e5a499568adaccd1a2beca9e8b80c76267024
vihaan1016 <vihaan1016@gmail.com>
2026-03-12 04:39:28 +0530

    feat: configure CORS to 5173

--- expense_tracker/backend/src/server.js
+++ expense_tracker/backend/src/server.js
@@ -3,15 +3,19 @@ import expenseRouter from './routes/expenseRouter.js'
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 dotenv.config();
+import cors from 'cors';
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+app.use(cors({ 
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(express.json());
 app.use('/api/expenses', expenseRouter);
-
 app.listen(PORT, ()=> {
     console.log(`server is running on port ${PORT}`);
 });
